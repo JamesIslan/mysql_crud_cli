7dbe69c68b585a5107f4ca926a1870539a3c0af6
Kaio Vitor <kaio.vitor@escolar.ifrn.edu.br>
2023-12-18 22:35:19 -0300

    Adição da funcionalidade de edição de registros

--- gui.py
+++ gui.py
@@ -3,11 +3,28 @@ from crud import DataBase
 
 def add(window,name,email,sex,phone):
     connect = DataBase('localhost', 'root', '123', 'aula_conexao_bd')
-    connect.create('cliente',fields=['nome', 'email','sexo', 'telefone'],values=[
-        name.value,email.value,sex.value,phone.value
-    ])
-    connect.close()
+    if '' in (name.value,email.value,sex.value,phone.value):
+        window.info(title='info', text='Informe valores para todos os atributos!')
+    else:
+        connect.create('cliente',fields=['nome', 'email','sexo', 'telefone'],values=[
+            name.value,email.value,sex.value,phone.value
+        ])
+        connect.close()
+        window.destroy()
+def edit(window,connection,reg,new_reg):
+    reg_modified = {'nome': new_reg[0].value, 'email': new_reg[1].value, 'sexo': new_reg[2].value, 'telefone': new_reg[3].value}
+    changed_values = [(k,v) for k,v in reg_modified.items() if not reg[k] == v]
+    for key in reg.keys():
+       if key == 'id_cliente':
+          pass
+       else:
+          connection.update('cliente',key,reg_modified[key],'id_cliente',reg['id_cliente'])
+    connection.close()
     window.destroy()
+    close_window_search()
+
+
+    
   
 def close_window_search():
     window_search_result.hide()
@@ -21,23 +38,49 @@ def remove(connection, reg):
 
 
 def window_add():
-    window = Window(app,width=300,height=250, layout='grid')
+    window = Window(app,width=400,height=250, layout='grid')
+    window.bg = '#EDE7DF'
     text_name = Text(window, text='Nome:', grid=[0,0])
-    input_name = TextBox(window, grid=[1,0])
+    input_name = TextBox(window, grid=[1,0], width=20)
     sex_name = Text(window,text='  Sexo:',grid=[2,0])
     sex_input = ButtonGroup(window,options=['m','f'],selected='m', grid=[3,0], horizontal=True)
     ghost_box = Box(window, height='20', grid=[0,1])
     text_email = Text(window, text='Email:', grid=[0,2])
-    input_email = TextBox(window,grid=[1,2])
+    input_email = TextBox(window,grid=[1,2], width=20)
     phone_text = Text(window,text='   Telefone:', grid=[2,2])
-    phone_input = TextBox(window,grid=[3,2])
-    button = PushButton(window,grid=[0,3],command=add,args=[window,input_name, input_email,sex_input,phone_input])
+    phone_input = TextBox(window,grid=[3,2], width=20)
+    ghost_box_3 = Box(window,grid=[0,3], height=50)
+    button = PushButton(window,grid=[2,4],text="Cadastrar",command=add,args=[window,input_name, input_email,sex_input,phone_input])
+    text_name.font = sex_name.font = text_email.font = phone_text.font = button.font= 'Calibri'
+    input_name.bg = input_email.bg = phone_input.bg = 'white'
+    button.bg = '#CB9888'
     window.tk.resizable(0,0)
     window.show()
 
 
-def edit():
-    pass
+def window_edit(connect,reg):
+    reg_dict = eval(reg.value)
+    window = Window(app,width=400,height=250, layout='grid')
+    window.bg = '#EDE7DF'
+    text_name = Text(window, text='Nome:', grid=[0,0])
+    name = TextBox(window, grid=[1,0], width=20,text=reg_dict['nome'])
+    sex_name = Text(window,text='  Sexo:',grid=[2,0])
+    sex = ButtonGroup(window,options=['m','f'],selected=reg_dict['sexo'], grid=[3,0], horizontal=True)
+    ghost_box = Box(window, height='20', grid=[0,1])
+    text_email = Text(window, text='Email:', grid=[0,2])
+    email = TextBox(window,grid=[1,2], width=20,text=reg_dict['email'])
+    phone_text = Text(window,text='   Telefone:', grid=[2,2])
+    phone= TextBox(window,grid=[3,2], width=20,text=reg_dict['telefone'])
+    ghost_box_3 = Box(window,grid=[0,3], height=50)
+    new_reg = [name,email,sex,phone]
+    button = PushButton(window,grid=[2,4],text="Cadastrar",command=edit,args=[window,connect,reg_dict,new_reg])
+    text_name.font = sex_name.font = email.font = phone_text.font = button.font= 'Calibri'
+    name.bg = email.bg = phone.bg = 'white'
+    button.bg = '#CB9888'
+    window.tk.resizable(0,0)
+    window.show()
+
+    
 
 
 def search(window):
@@ -62,7 +105,7 @@ def search(window):
         ghost_box = Box(window_search_result,width='fill', height=50)
         box_options = Box(window_search_result,width='fill', height=50,layout='grid')
         ghost_box_options = Box(box_options, grid=[0, 0], width=250)
-        button_edit = PushButton(box_options, text="Editar", command=edit,grid=[1,0])
+        button_edit = PushButton(box_options, text="Editar", command=window_edit,grid=[1,0], args=[connect,listbox])
         button_remove = PushButton(box_options, text="Excluir", command=remove, grid=[2, 0], args=[connect,listbox])
         button_edit.bg = button_remove.bg = "#CB9888"
         button_edit.font = button_remove.font =  "Calibri"
